ad2f735d92506075c056b4b37fe67ed941ecb0d5
Justin Scheiner <emdeefive@gmail.com>
2011-12-31 17:10:42 -0500

    Clean up by actually using the constants.

--- prefetch.c
+++ prefetch.c
@@ -16,67 +16,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef void(*op_t)(long *, uint64_t, uint64_t);
-
-/**
- * Looks n fibonacci numbers ahead, and calls op_highest with the value,
- * then op with the current fibonacci number.
- */
-void fibo_lookahead_foreach(long *data, uint64_t len, int iters,
-    op_t op_highest, op_t op_lowest, int n) {
-  uint64_t *buf = calloc(n, sizeof(uint64_t));
-  int i_buf = 0, i;
-
-  // Fill the first n elements of the fibo buffer.
-  // To avoid expensive %'s buf[i] < n for all i.
-  uint64_t index, follow, temp;
-  for (i_buf = 0, index = 1, follow = 0; i_buf < n; i_buf++) {
-    buf[i_buf] = index;
-    temp = index;
-    index += follow;
-    follow = temp;
-    while (index >= len) {
-      index -= len;
-    }
-  }
-
-  for (i = 0, i_buf = 0; i < iters; i++) {
-    // i_buf always points at the lowest number in the buffer.
-    op_lowest(data, len, buf[i_buf]);
-
-    int prev = dec_ring(i_buf, n);
-    buf[i_buf] = buf[prev] + buf[dec_ring(prev, n)];
-    while (buf[i_buf] >= len) {
-      buf[i_buf] -= len;
-    }
-    op_highest(data, len, buf[i_buf]);
-    i_buf = inc_ring(i_buf, n);
-  }
-  free(buf);
-}
-
-int inc_ring(int i, int n) {
-  int next = i + 1;
-  return next >= n ? 0 : next;
-}
-
-int dec_ring(int i, int n) {
-  int next = i + n - 1;
-  return next >= n ? next - n : next;
-}
-
-void prefetch(long *data, uint64_t len, uint64_t index) {
-  __builtin_prefetch(&data[index]);
-}
-
-void increment(long *data, uint64_t len, uint64_t index) {
-  data[index]++;
-}
-
-void nop(long *data, uint64_t len, uint64_t index) {
-  return;
-}
-
 // 1G (Significantly greater than cache size).
 #define LEN 0x8000000
 
@@ -86,6 +25,66 @@ void nop(long *data, uint64_t len, uint64_t index) {
 // Experimentally works well.
 #define NAHEAD 32
 
+typedef void(*op_t)(long *, uint64_t);
+
+/**
+ * Looks n fibonacci numbers ahead, and calls op_highest with the value,
+ * then op with the current fibonacci number.
+ */
+void fibo_lookahead_foreach(long *data, op_t op_highest, op_t op_lowest) {
+  uint64_t *buf = calloc(NAHEAD, sizeof(uint64_t));
+  int i_buf = 0, i;
+
+  // Fill the first n elements of the fibo buffer.
+  // To avoid expensive %'s buf[i] < n for all i.
+  uint64_t index, follow, temp;
+  for (i_buf = 0, index = 1, follow = 0; i_buf < NAHEAD; i_buf++) {
+    buf[i_buf] = index;
+    temp = index;
+    index += follow;
+    follow = temp;
+    while (index >= LEN) {
+      index -= LEN;
+    }
+  }
+
+  for (i = 0, i_buf = 0; i < ITERS; i++) {
+    // i_buf always points at the lowest number in the buffer.
+    op_lowest(data, buf[i_buf]);
+
+    int prev = dec_ring(i_buf);
+    buf[i_buf] = buf[prev] + buf[dec_ring(prev)];
+    while (buf[i_buf] >= LEN) {
+      buf[i_buf] -= LEN;
+    }
+    op_highest(data, buf[i_buf]);
+    i_buf = inc_ring(i_buf);
+  }
+  free(buf);
+}
+
+int inc_ring(int i) {
+  int next = i + 1;
+  return next >= NAHEAD ? 0 : next;
+}
+
+int dec_ring(int i) {
+  int next = i + NAHEAD - 1;
+  return next >= NAHEAD ? next - NAHEAD : next;
+}
+
+void prefetch(long *data, uint64_t index) {
+  __builtin_prefetch(&data[index]);
+}
+
+void increment(long *data, uint64_t index) {
+  data[index]++;
+}
+
+void nop(long *data, uint64_t index) {
+  return;
+}
+
 int main(int argc, const char *argv[]) {
   // Use totally separate chunks so the cache is cold at the start of both.
   long *data0 = calloc(LEN, sizeof(long));
@@ -94,7 +93,7 @@ int main(int argc, const char *argv[]) {
   double avg;
 
   start = clock();
-  fibo_lookahead_foreach(data0, LEN, ITERS, nop, increment, NAHEAD);
+  fibo_lookahead_foreach(data0, nop, increment);
   end = clock();
 
   printf("CPU time w/o prefetching: %f\n",
@@ -102,7 +101,7 @@ int main(int argc, const char *argv[]) {
 
   // Try with prefetching.
   start = clock();
-  fibo_lookahead_foreach(data1, LEN, ITERS, prefetch, increment, NAHEAD);
+  fibo_lookahead_foreach(data1, prefetch, increment);
   end = clock();
 
   printf("CPU time prefetching %d ahead: %f\n",
